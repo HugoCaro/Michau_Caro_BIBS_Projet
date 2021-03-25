6e83ddc2c624cc72aa377c83b318890682cd6975
ThomasSMichau <thomas.michau@universite-paris-saclay.fr>
2021-03-25 16:47:09 +0100

    modif construct

--- src/ARN.java
+++ src/ARN.java
@@ -6,9 +6,14 @@ public class ARN {
         this.seq="";
         this.struct="";
     }
-    public ARN( String seq, String struct){
-        this.seq=seq;
-        this.struct=struct;
+    public ARN( String seq, String struct) {
+        if (seq.length() == struct.length()) {
+            this.seq = seq;
+            this.struct = struct;
+        }
+        else{
+            System.out.println("gros nul");
+        }
     }
 
     public Boolean compareStruct(ARN b){
@@ -16,5 +21,17 @@ public class ARN {
     }
     public Boolean compareSeq(ARN b){
         return this.seq.equals(b.seq) && this.struct.equals(b.struct);
+    }
+
+    public static void main(String[] args) {
+        ARN a = new ARN("AUGGGC","--((--");
+        ARN b = new ARN("AUGGGC","--((--");
+        ARN c = new ARN("AUGGGC","--(())");
+        ARN d = new ARN("AUOOGC","--((--");
+        ARN e = new ARN("AUPMLJ","--(+--");
+        System.out.println(a.compareStruct(b));
+        System.out.println(a.compareStruct(c));
+        System.out.println(a.compareStruct(d));
+        System.out.println(a.compareStruct(e));
     }
 }
