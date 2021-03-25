363262cfce4a8bd652cf77759cdbf66533397507
ThomasSMichau <thomas.michau@universite-paris-saclay.fr>
2021-03-25 16:21:51 +0100

    constructeur + atribut

--- src/ARN.java
+++ src/ARN.java
@@ -1,2 +1,13 @@
 public class ARN {
+    private String seq;
+    private String struct;
+
+    public ARN(){
+        this.seq="";
+        this.struct="";
+    }
+    public ARN( String seq, String struct){
+        this.seq=seq;
+        this.struct=struct;
+    }
 }
