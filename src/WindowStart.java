c3d82145fa68ae53bc5b68e025d2302a7e8f5abe
ThomasSMichau <thomas.michau@universite-paris-saclay.fr>
2021-03-29 09:18:08 +0200

    C'est un test, tkt y a rien qui marche, juste de la prise en main

--- src/WindowStart.java
+++ src/WindowStart.java
@@ -8,9 +8,12 @@ public class WindowStart {
         f.setSize(300, 300);
         JPanel pannel = new JPanel();
 
-        JTextArea  textArea1 = new JTextArea ("mon texte");
+        JTextArea  textArea1 = new JTextArea ("Entrer une séquence ARN");
+        textArea1.setSize(200, 100);
+        JTextArea  textArea2 = new JTextArea ("Entrer une structure ARN");
 
         pannel.add(textArea1);
+        pannel.add(textArea2);
         f.getContentPane().add(pannel);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
