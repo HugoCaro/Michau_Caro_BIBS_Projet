e568f0c19637f5a99b01d198c3778cdf7b5e15b2
ThomasSMichau <thomas.michau@universite-paris-saclay.fr>
2021-03-29 11:43:36 +0200

    champs nom + bouton

--- src/WindowStart.java
+++ src/WindowStart.java
@@ -5,28 +5,59 @@ import java.awt.event.ActionListener;
 
 public class WindowStart {
 
-    private JFrame frameStart = new JFrame ("Saisi ARN");
+    private JFrame frameStart = new JFrame ("Saisie ARN");
+
     private JButton buttonAdd = new JButton("Ajout de la séquence");
+
+    private JPanel panNom = new JPanel();
+    public JTextArea txtNom= new JTextArea("Entrer un identifiant de \nla séquence ARN");
+    private JLabel labNom= new JLabel("ID : ");
+
     private JPanel panARN = new JPanel();
-    private JTextArea txtARN= new JTextArea("Entrer une \n\tséquence ARN");;
+    public JTextArea txtARN= new JTextArea("Entrer une séquence ARN");
     private JLabel labARN= new JLabel("Séquence : ");
+
     private JPanel panStrc= new JPanel();
-    private JTextArea txtStrc= new JTextArea("Entrer une \n\tstructure ARN");;
+    public JTextArea txtStrc= new JTextArea("Entrer une structure ARN");
     private JLabel labStrc= new JLabel("Strcuture : ");
 
     public WindowStart() {
 
+        txtNom = this.txtNom;
+        this.labNom.setHorizontalAlignment(SwingConstants.CENTER);
+        this.panNom.setLayout(new GridLayout(1,2));
+        this.panNom.add(this.labNom);
+        this.panNom.add(txtNom,1);
+
+        txtARN = this.txtARN;
+        this.labARN.setHorizontalAlignment(SwingConstants.CENTER);
         this.panARN.setLayout(new GridLayout(1,2));
         this.panARN.add(this.labARN);
-        this.panARN.add(this.txtARN);
+        this.panARN.add(txtARN,1);
 
+        txtStrc = this.txtStrc;
+        this.labStrc.setHorizontalAlignment(SwingConstants.CENTER);
         this.panStrc.setLayout(new GridLayout(1,2));
         this.panStrc.add(this.labStrc);
-        this.panStrc.add(this.txtStrc,1);
+        this.panStrc.add(txtStrc,1);
 
-        this.frameStart.setLayout(new GridLayout(3,1));
+        this.buttonAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String nomARN = txtNom.getText();
+                String seq = txtARN.getText().toUpperCase() ;
+                String strc = txtARN.getText().toUpperCase() ;
+                System.out.println(nomARN);
+                System.out.println(seq);
+                System.out.println(strc);
+            }
+        });
+
+        this.frameStart.setLayout(new GridLayout(4,1));
+        this.frameStart.add(this.panNom);
         this.frameStart.add(this.panARN);
-        this.frameStart.add(this.panStrc,1);
+        this.frameStart.add(this.panStrc);
+        this.frameStart.add(this.buttonAdd);
         this.frameStart.setSize(400,300);
         this.frameStart.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frameStart.setVisible(true);
